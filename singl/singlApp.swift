6c2dab36fef18ba17d09f7ef669cfaa7a6250971
Aang Muammar Zein <amuammarzein@gmail.com>
2023-07-17 14:58:46 +0700

    Initial Commit

---
+++ singl/singlApp.swift
@@ -0,0 +1,17 @@
+//
+//  singlApp.swift
+//  singl
+//
+//  Created by Aang Muammar Zein on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct singlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
