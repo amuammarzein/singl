7a7e9138721702b192d7f55d5bce356937ce6159
Patrick Sinaga <sinagapatrick610@gmail.com>
2023-07-20 17:22:38 +0700

    update feature share in vocalrangeresult
    sudah bisa share ke ig,airdrop,dll

--- singl/View/VocalRangesTestResultView.swift
+++ singl/View/VocalRangesTestResultView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct VocalRangesTestResultView: View {
     @StateObject var taskManager:TaskManager = TaskManager()
+    @State var isShow = false
+    @State var items: [Any] = []
+    
     var body: some View {
         if(taskManager.isNext){
             DashboardView()
@@ -31,16 +34,7 @@ struct VocalRangesTestResultView: View {
                     Spacer()
                 }.padding(.top,10).padding(.leading,30)
                 ScrollView(){
-                    ZStack(){
-                                            Image("TestResultCard").resizable().scaledToFit().frame(maxWidth:.infinity).padding(0)
-                                            VStack(spacing:10){
-                                                Text("Your Vocal Range").font(.title3).bold()
-                                                Text(taskManager.vocalRange).font(.title).bold()
-                                                Spacer()
-                                                Text("Your Vocal Type").font(.title3).bold()
-                                                Text(taskManager.vocalType).font(.title).bold()
-                                            }.padding(30).padding(.vertical,CGFloat(UIScreen.main.bounds.width/3))
-                                        }
+                    cardResultView
                     VStack(){
                         if(taskManager.isTest){
                             VStack(){
@@ -63,9 +57,22 @@ struct VocalRangesTestResultView: View {
                                 Button(
                                     action:{
                                         taskManager.isShareTrue()
+                                        self.items.removeAll()
+                                        
+                                        guard let resultImage = ImageRenderer(content: cardResultView).uiImage else {
+                                            return
+                                        }
+                                        self.items.append(resultImage)
+                                        self.isShow.toggle()
+                                        
+                                        print("Berhasil!")
                                     }){
                                         Text("Share to your story!").foregroundColor(Color("Blue")).font(.body).bold().padding(20)
                                     }.frame(maxWidth:.infinity).background(.white).cornerRadius(30)
+                                
+                                    .sheet(isPresented: $isShow) {
+                                        ShareSheetView(items: items).edgesIgnoringSafeArea(.all)
+                                    }
                                 
                             }.padding(30)
                             
@@ -77,7 +84,32 @@ struct VocalRangesTestResultView: View {
         }
         
     }
+    
+    var cardResultView: some View {
+        ZStack(){
+                                Image("TestResultCard").resizable().scaledToFit().frame(maxWidth:.infinity).padding(0)
+                                VStack(spacing:10){
+                                    Text("Your Vocal Range").font(.title3).bold()
+                                    Text(taskManager.vocalRange).font(.title).bold()
+                                    Spacer()
+                                    Text("Your Vocal Type").font(.title3).bold()
+                                    Text(taskManager.vocalType).font(.title).bold()
+                                }.padding(30).padding(.vertical,CGFloat(UIScreen.main.bounds.width/3))
+                            }
+    }
 }
+
+struct ShareSheetView: UIViewControllerRepresentable {
+    let items: [Any]
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return UIActivityViewController(activityItems: items, applicationActivities: nil)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
 
 struct VocalRangesTestResultView_Previews: PreviewProvider {
     static var previews: some View {
